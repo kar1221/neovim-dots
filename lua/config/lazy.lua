7688ba14a7ba9488c805eb59109bfaba324e9916
kar1221 <khyeong191@gmail.com>
2024-06-23 19:30:47 +0800

    i am stupid
    PURE STUPIDITY

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,9 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.coding.codeium" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.test.core" },
+    { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
   },
   defaults = {
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -65,9 +65,6 @@ return {
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "roobert/tailwindcss-colorizer-cmp.nvim",
   },
 
   event = "InsertCharPre",
