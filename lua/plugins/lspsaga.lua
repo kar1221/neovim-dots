829645def94cf791649b2bc2a4e0cb57e3ae6de8
kar1221 <khyeong191@gmail.com>
2024-06-24 14:34:43 +0800

    add lspsaga

---
+++ lua/plugins/lspsaga.lua
@@ -0,0 +1,20 @@
+return {
+  "nvimdev/lspsaga.nvim",
+  config = function()
+    require("lspsaga").setup({})
+  end,
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  keys = {
+    {
+      mode = { "n" },
+      "<leader>k",
+      "<cmd>Lspsaga peek_type_definition<cr>",
+      desc = "Peek Definition",
+    },
+  },
+}
